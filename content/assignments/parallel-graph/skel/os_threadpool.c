79f9a37718f9c57e6d22ce649183cb10010a5dce
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2023-04-28 13:50:00 +0300

    Linting

--- content/assignments/parallel-graph/skel/os_threadpool.c
+++ content/assignments/parallel-graph/skel/os_threadpool.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include "os_threadpool.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,51 +10,55 @@
 /* Creates a task that thread must execute */
 os_task_t *task_create(void *arg, void (*f)(void *))
 {
-    os_task_t *task = (os_task_t*)malloc(sizeof(os_task_t));
-    task->argument = arg;
-    task->task = f;
-    return task;
+	os_task_t *task = (os_task_t *)malloc(sizeof(os_task_t));
+
+	task->argument = arg;
+	task->task = f;
+	return task;
 }
 
 /* Add a new task to threadpool task queue */
 void add_task_in_queue(os_threadpool_t *tp, os_task_t *t)
 {
-    os_task_queue_t *node = (os_task_queue_t*)malloc(sizeof(os_task_queue_t));
-    node->task = t;
-    node->next = NULL;
-    // make adding to queue atomic
-    pthread_mutex_lock(&tp->taskLock);
-    // If head is null set it to node
-    if (tp->tasks == NULL) {
-        tp->tasks = node;
-    } else {
-        // Otherwise iterate to the end of the list and add node
-        os_task_queue_t *current = tp->tasks;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        current->next = node;
-    }
-    pthread_mutex_unlock(&tp->taskLock);
+	os_task_queue_t *node = (os_task_queue_t *)malloc(sizeof(os_task_queue_t));
+
+	node->task = t;
+	node->next = NULL;
+	// make adding to queue atomic
+	pthread_mutex_lock(&tp->taskLock);
+	// If head is null set it to node
+	if (tp->tasks == NULL) {
+		tp->tasks = node;
+	} else {
+		// Otherwise iterate to the end of the list and add node
+		os_task_queue_t *current = tp->tasks;
+
+		while (current->next != NULL)
+			current = current->next;
+		current->next = node;
+	}
+	pthread_mutex_unlock(&tp->taskLock);
 }
 
 /* Get the head of task queue from threadpool */
 os_task_t *get_task(os_threadpool_t *tp)
 {
-    // make getting from queue atomic
-    pthread_mutex_lock(&tp->taskLock);
-    os_task_queue_t *node = tp->tasks;
-    // If head is null return null
-    if (node == NULL) {
-        pthread_mutex_unlock(&tp->taskLock);
-        return NULL;
-    }
-    // Set head to the node next to the head
-    tp->tasks = node->next;
-    os_task_t *task = node->task;
-    pthread_mutex_unlock(&tp->taskLock);
-    free(node);
-    return task;
+	// make getting from queue atomic
+	pthread_mutex_lock(&tp->taskLock);
+	os_task_queue_t *node = tp->tasks;
+
+	// If head is null return null
+	if (node == NULL) {
+		pthread_mutex_unlock(&tp->taskLock);
+		return NULL;
+	}
+	// Set head to the node next to the head
+	tp->tasks = node->next;
+	os_task_t *task = node->task;
+
+	pthread_mutex_unlock(&tp->taskLock);
+	free(node);
+	return task;
 }
 
 /* === THREAD POOL === */
@@ -60,54 +66,57 @@ os_task_t *get_task(os_threadpool_t *tp)
 /* Initialize the new threadpool */
 os_threadpool_t *threadpool_create(unsigned int nTasks, unsigned int nThreads)
 {
-    // Allocate memory for threadpool
-    os_threadpool_t *tp = (os_threadpool_t*)malloc(sizeof(os_threadpool_t));
-    tp->should_stop = 0;
-    tp->num_threads = nThreads;
-    tp->tasks = NULL;
-    // Initialize mutex
-    pthread_mutex_init(&tp->taskLock, NULL);
-    // Create threads
-    tp->threads = (pthread_t*)malloc(sizeof(pthread_t) * nThreads);
-    for (unsigned int i = 0; i < nThreads; i++) {
-        int result = pthread_create(&tp->threads[i], NULL, thread_loop_function, tp);
-        if (result != 0) {
-            printf("Failed to create thread.\n");
-        }
-    }
-    return tp;
+	// Allocate memory for threadpool
+	os_threadpool_t *tp = (os_threadpool_t *)malloc(sizeof(os_threadpool_t));
+
+	tp->should_stop = 0;
+	tp->num_threads = nThreads;
+	tp->tasks = NULL;
+	// Initialize mutex
+	pthread_mutex_init(&tp->taskLock, NULL);
+	// Create threads
+	tp->threads = (pthread_t *)malloc(sizeof(pthread_t) * nThreads);
+	for (unsigned int i = 0; i < nThreads; i++) {
+		int result = pthread_create(&tp->threads[i], NULL, thread_loop_function, tp);
+
+		if (result != 0)
+			printf("Failed to create thread.\n");
+	}
+	return tp;
 }
 
 /* Loop function for threads */
 void *thread_loop_function(void *args)
 {
-    os_threadpool_t *tp = (os_threadpool_t*)args;
-    while (tp->should_stop == 0) {
-        os_task_t *task = get_task(tp);
-        // Run task
-        if (task != NULL) {
-            task->task(task->argument);
-            free(task);
-        }
-    }
-    return NULL;
+	os_threadpool_t *tp = (os_threadpool_t *)args;
+
+	while (tp->should_stop == 0) {
+		os_task_t *task = get_task(tp);
+
+		// Run task
+		if (task != NULL) {
+			task->task(task->argument);
+			free(task);
+		}
+	}
+	return NULL;
 }
 
 /* Stop the thread pool once a condition is met */
 void threadpool_stop(os_threadpool_t *tp, int (*processingIsDone)(os_threadpool_t *))
 {
-    while (1) {
-        // Check if there are no more tasks and if processing is done
-        if (tp->tasks == NULL && processingIsDone(tp) == 1) {
-            tp->should_stop = 1;
-            // Join threads
-            for (unsigned int i = 0; i < tp->num_threads; i++) {
-                int result = pthread_join(tp->threads[i], NULL);
-                if (result != 0) {
-                    printf("Failed to join thread.\n");
-                }
-            }
-            break;
-        }
-    }
+	while (1) {
+		// Check if there are no more tasks and if processing is done
+		if (tp->tasks == NULL && processingIsDone(tp) == 1) {
+			tp->should_stop = 1;
+			// Join threads
+			for (unsigned int i = 0; i < tp->num_threads; i++) {
+				int result = pthread_join(tp->threads[i], NULL);
+
+				if (result != 0)
+					printf("Failed to join thread.\n");
+			}
+			break;
+		}
+	}
 }
--- content/assignments/parallel-graph/skel/parallel.c
+++ content/assignments/parallel-graph/skel/parallel.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,95 +13,99 @@
 #define MAX_TASK 100
 #define MAX_THREAD 4
 
-int sum = 0;
+int sum;
 os_graph_t *graph;
 os_threadpool_t *tp;
-
 pthread_mutex_t sum_mutex;
 pthread_mutex_t neighbor_mutex;
 
 void processNode(void *arg)
 {
-    unsigned int nodeIdx = *(unsigned int*)arg;
-    // get index and update sum
-    os_node_t *node = graph->nodes[nodeIdx];
-    // addition is not atomic
-    pthread_mutex_lock(&sum_mutex);
-    sum += node->nodeInfo;
-    pthread_mutex_unlock(&sum_mutex);
-    // for each unvisited neighbor start a new task
-    for (int i = 0; i < node->cNeighbours; i++) {
-        // this is not atomic. mark node as visited, but not complete
-        pthread_mutex_lock(&neighbor_mutex);
-        int visited = graph->visited[node->neighbours[i]];
-        if (visited == 0)
-            graph->visited[node->neighbours[i]] = 1;
-        pthread_mutex_unlock(&neighbor_mutex);
-        // create task
-        if (visited == 0) {
-            os_task_t *task = task_create(&node->neighbours[i], processNode);
-            add_task_in_queue(tp, task);
-        }
-    }
-    // mark node as complete
-    graph->visited[nodeIdx] = 2;
+	unsigned int nodeIdx = *(unsigned int *)arg;
+	// get index and update sum
+	os_node_t *node = graph->nodes[nodeIdx];
 
-    // find other conex components by starting a new task for a random unvisited node
-    for (int i = 0; i < graph->nCount; i++) {
-        pthread_mutex_lock(&neighbor_mutex);
-        int visited = graph->visited[i];
-        if (visited == 0)
-            graph->visited[i] = 1;
-        pthread_mutex_unlock(&neighbor_mutex);
-        if (visited == 0) {
-            os_task_t *task = task_create(&graph->nodes[i]->nodeID, processNode);
-            add_task_in_queue(tp, task);
-            break;
-        }
-    }
+	// addition is not atomic
+	pthread_mutex_lock(&sum_mutex);
+	sum += node->nodeInfo;
+	pthread_mutex_unlock(&sum_mutex);
+	// for each unvisited neighbor start a new task
+	for (int i = 0; i < node->cNeighbours; i++) {
+		// this is not atomic. mark node as visited, but not complete
+		pthread_mutex_lock(&neighbor_mutex);
+		int visited = graph->visited[node->neighbours[i]];
+
+		if (visited == 0)
+			graph->visited[node->neighbours[i]] = 1;
+		pthread_mutex_unlock(&neighbor_mutex);
+		// create task
+		if (visited == 0) {
+			os_task_t *task = task_create(&node->neighbours[i], processNode);
+
+			add_task_in_queue(tp, task);
+		}
+	}
+	// mark node as complete
+	graph->visited[nodeIdx] = 2;
+	// find other conex components by starting a new task for a random unvisited node
+	for (int i = 0; i < graph->nCount; i++) {
+		pthread_mutex_lock(&neighbor_mutex);
+		int visited = graph->visited[i];
+
+		if (visited == 0)
+			graph->visited[i] = 1;
+		pthread_mutex_unlock(&neighbor_mutex);
+		if (visited == 0) {
+			os_task_t *task = task_create(&graph->nodes[i]->nodeID, processNode);
+
+			add_task_in_queue(tp, task);
+			break;
+		}
+	}
 }
 
-int graphDone(os_threadpool_t* tp)
+int graphDone(os_threadpool_t *tp)
 {
-    // graph is done if all the nodes are marked as complete
-    for (int i = 0; i < graph->nCount; i++)
-        if (graph->visited[i] == 0 || graph->visited[i] == 1)
-            return 0;
-    return 1;
+	// graph is done if all the nodes are marked as complete
+	for (int i = 0; i < graph->nCount; i++)
+		if (graph->visited[i] == 0 || graph->visited[i] == 1)
+			return 0;
+	return 1;
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Usage: ./main input_file\n");
-        exit(1);
-    }
+	if (argc != 2) {
+		printf("Usage: ./parallel input_file\n");
+		exit(1);
+	}
 
-    FILE *input_file = fopen(argv[1], "r");
+	FILE *input_file = fopen(argv[1], "r");
 
-    if (input_file == NULL) {
-        printf("[Error] Can't open file\n");
-        return -1;
-    }
+	if (input_file == NULL) {
+		printf("[Error] Can't open file\n");
+		return -1;
+	}
 
-    graph = create_graph_from_file(input_file);
-    if (graph == NULL) {
-        printf("[Error] Can't read the graph from file\n");
-        return -1;
-    }
+	graph = create_graph_from_file(input_file);
+	if (graph == NULL) {
+		printf("[Error] Can't read the graph from file\n");
+		return -1;
+	}
 
-    // init sync variables
-    pthread_mutex_init(&sum_mutex, NULL);
-    pthread_mutex_init(&neighbor_mutex, NULL);
-    // create thread pool
-    tp = threadpool_create(MAX_TASK, MAX_THREAD);
-    graph->visited[0] = 1;
-    // create task for first node and add it
-    os_task_t *task = task_create(&graph->nodes[0]->nodeID, processNode);
-    add_task_in_queue(tp, task);
-    // wait for all tasks to finish
-    threadpool_stop(tp, graphDone);
-    printf("%d", sum);
-    return 0;
+	sum = 0;
+	// init sync variables
+	pthread_mutex_init(&sum_mutex, NULL);
+	pthread_mutex_init(&neighbor_mutex, NULL);
+	// create thread pool
+	tp = threadpool_create(MAX_TASK, MAX_THREAD);
+	graph->visited[0] = 1;
+	// create task for first node and add it
+	os_task_t *task = task_create(&graph->nodes[0]->nodeID, processNode);
+
+	add_task_in_queue(tp, task);
+	// wait for all tasks to finish
+	threadpool_stop(tp, graphDone);
+	printf("%d", sum);
+	return 0;
 }
